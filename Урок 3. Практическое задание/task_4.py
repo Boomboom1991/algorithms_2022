bfafec474ccb9dca7ac005d893be2ee5203a4cee
Vladislav <vladislav.kapralov@gmail.com>
2022-02-13 19:59:01 +0300

    HW_3 made

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -14,3 +14,32 @@
 и одного из алгоритмов, например, sha512
 Можете усложнить задачу, реализовав ее через ООП
 """
+
+import hashlib
+import string
+import secrets
+
+url_bd = {'vk.com': '1ff232f32',
+          'facebook.com': 'f12f12f1',
+          'dodo.ru': 'somehash'}
+
+
+def crypto_salt():
+    return ''.join(secrets.choice(string.ascii_letters + string.digits) for i in range(5))
+
+
+def cash_url(url):
+    return hashlib.sha512(url.encode() + crypto_salt().encode()).hexdigest()
+
+
+def main():
+    url = input("Введите URl: ")
+    if url_bd.get(url) is not None:
+        print("Хеш данного URL: ", url_bd.get(url))
+    else:
+        url_bd.setdefault(url, cash_url(url))
+    print(url_bd)
+
+
+if __name__ == "__main__":
+    main()
